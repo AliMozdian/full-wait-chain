b8ce2d0640e62d53d8cca329335be198e2a5f80a
Ali <alimozdian@gmail.com>
2026-07-08 11:45:59 +0330

    added count to edges, const values for WakeEvent attr

--- model/wake_event.py
+++ model/wake_event.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass
 from .process import Process
 
 
-@dataclass
+@dataclass(frozen=True)
 class WakeEvent:
     waker: Process
     target: Process
--- model/wake_graph.py
+++ model/wake_graph.py
@@ -22,8 +22,17 @@ class WakeGraph:
             process=event.target,
         )
 
-        self.graph.add_edge(
-            event.waker.pid,
-            event.target.pid,
-            weight=event.offcpu_time_us,
-        )
+        if self.graph.has_edge(event.waker.pid, event.target.pid):
+
+            edge = self.graph[event.waker.pid][event.target.pid]
+            edge["count"] += 1
+            edge["total_offcpu_time"] += event.offcpu_time_us
+
+        else:
+
+            self.graph.add_edge(
+                event.waker.pid,
+                event.target.pid,
+                count=1,
+                total_offcpu_time=event.offcpu_time_us,
+            )
